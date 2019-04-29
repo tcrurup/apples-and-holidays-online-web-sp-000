14c3a17cc418872d93482fea6137a643ed8eeb74
Anthony Rurup <tcrurup@gmail.com>
2019-04-29 20:27:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   holidays_with_bbq = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday_symbol, supplies_needed|
-      supplies_needed.include?("BBQ") ? holidays_with_bbq << holiday_symbol : false
+      if supplies_needed.include?("BBQ") { holidays_with_bbq << holiday_symbol }
     end
   end
   holidays_with_bbq
